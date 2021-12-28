63ad8e2fbe4b1fe7f356d185f838498e7f604c96
wander23 <57674214+wander23@users.noreply.github.com>
2021-12-28 21:59:06 +0700

    add be, phieugiamgia

--- Source/components/account/account.model.js
+++ Source/components/account/account.model.js
@@ -4,14 +4,25 @@ module.exports = function () {
   this.page = async (id, result) => {
     try {
       const pool = await conn;
-      const sqlstring = "select * from khachhang where MaKhachHAng = @varID";
-      return await pool
-        .request()
-        .input("varID", sql.Int, id)
-        .query(sqlstring, (e, data) => {
-          if (data.recordset.length > 0) result(null, data.recordset[0]);
-          else result(true, null);
-        });
+      
+      const sqlstring1 = "select * from khachhang where MaKhachHAng = @varID";
+      const ttkh = await pool.request()
+      .input("varID", sql.Int, id).query(sqlstring1);
+
+      const sqlstring2 = "select * from be where khachhang = @varID";
+      const be = await pool.request()
+      .input("varID", sql.Int, id).query(sqlstring2);
+
+      const sqlstring3 = "select * from PhieuGiamGia where khachhang = @varID";
+      const pgg = await pool.request()
+      .input("varID", sql.Int, id).query(sqlstring3);
+
+      result(false, {
+        thongtinkhachang: ttkh.recordset[0],
+        be: be.recordset,
+        phieugiamgia: pgg.recordset
+      });
+
     } catch {
       result(true, null);
     }
