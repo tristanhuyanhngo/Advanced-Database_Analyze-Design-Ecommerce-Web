d5b259e02264f1bccd8d3d01c1d8d15d2e3403c0
Nguyen Huy Anh Thư <63990707+NguyenHuyAnhThu@users.noreply.github.com>
2021-12-28 20:23:24 +0700

    up

--- Source/app.js
+++ Source/app.js
@@ -1,58 +1,63 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const flash = require('express-flash')
-const session = require('express-session')
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const flash = require("express-flash");
+const session = require("express-session");
 
-const indexRouter = require('./components/index/index.route')
-const usersRouter = require('./components/user/user.route');
-const cartRouter = require('./components/cart/cart.route');
-const checkoutRouter = require('./routes/checkout');
-const loginRouter = require('./components/login/login.route');
-const orderhistoryRouter = require('./routes/oderhistory');
-const productdetailRouter = require('./routes/product-details');
-const accountRouter = require('./components/account/account.route');
+const indexRouter = require("./components/index/index.route");
+const usersRouter = require("./components/user/user.route");
+const cartRouter = require("./components/cart/cart.route");
+const checkoutRouter = require("./routes/checkout");
+const loginRouter = require("./components/login/login.route");
+const orderhistoryRouter = require("./routes/oderhistory");
+const productdetailRouter = require("./routes/product-details");
+const accountRouter = require("./components/account/account.route");
+const editpro5Router = require("./routes/editpro5");
+const orderRouter = require("./routes/order-detail");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({ secret: "hfoasfauisfoas", resave: false, saveUninitialized: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({ secret: "hfoasfauisfoas", resave: false, saveUninitialized: false })
+);
+app.use(express.static(path.join(__dirname, "public")));
 app.use(flash());
 
-app.use('/', indexRouter);
-app.use('/cart', cartRouter);
-app.use('/checkout', checkoutRouter);
-app.use('/login', loginRouter);
-app.use('/orderhistory', orderhistoryRouter);
-app.use('/product-details', productdetailRouter);
-app.use('/users', usersRouter);
-app.use('/account', accountRouter);
-
+app.use("/", indexRouter);
+app.use("/cart", cartRouter);
+app.use("/checkout", checkoutRouter);
+app.use("/login", loginRouter);
+app.use("/orderhistory", orderhistoryRouter);
+app.use("/product-details", productdetailRouter);
+app.use("/users", usersRouter);
+app.use("/account", accountRouter);
+app.use("/editpro5", editpro5Router);
+app.use("/orderdetail", orderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
