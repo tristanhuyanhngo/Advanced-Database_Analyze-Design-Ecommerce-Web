7c1840a9b34a9bee9ecbc21576a2aa9095aa486a
wander23 <57674214+wander23@users.noreply.github.com>
2021-12-28 22:04:30 +0700

    up

--- Source/components/account/account.controller.js
+++ Source/components/account/account.controller.js
@@ -4,7 +4,11 @@ const store = require("store");
 
 exports.paging = (req, res) => {
   accountLogin.page(store.get("id"), (e, data) => {
-    if (!e) res.render("account", { user: data });
+    if (!e) res.render("account", { 
+      thongtinkhachang: data.thongtinkhachang,
+      be: data.be,
+      phieugiamgia:  data.phieugiamgia
+    });
     else res.redirect("/");
   });
 };
